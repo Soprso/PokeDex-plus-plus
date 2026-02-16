fa5b08e0fd918e8387e7a0824c1f70e97d82393c
soprso <ghoshsoumyadeep3@gmail.com>
2026-02-17 01:11:28 +0530

    UI: integrate Google Analytics tracking

--- pokedex-web/src/App.tsx
+++ pokedex-web/src/App.tsx
@@ -1,11 +1,28 @@
 import { ClerkProvider } from '@clerk/clerk-react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import PokeHub from './pages/PokeHub';
 import PokemonDetails from './pages/PokemonDetails';
 import Profile from './pages/Profile';
 import Shop from './pages/Shop';
 import SSOCallback from './pages/SSOCallback';
+
+// Google Analytics Tracker Component
+function AnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const gtag = (window as any).gtag;
+    if (typeof gtag === 'function') {
+      gtag('config', 'G-XXXXXXXXXX', {
+        page_path: location.pathname + location.search,
+      });
+    }
+  }, [location]);
+
+  return null;
+}
 
 // Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY || import.meta.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
@@ -18,6 +35,7 @@ function App() {
   return (
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/" signInUrl="/">
       <BrowserRouter>
+        <AnalyticsTracker />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/details/:name" element={<PokemonDetails />} />
